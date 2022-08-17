8245e5ef849f9f36654e22a63ab3096bd5b517d2
ldqsangvn2103 <ldqsangvn2103@gmail.com>
2022-08-18 03:33:43 +0700

    13

--- index.php
+++ index.php
@@ -120,9 +120,6 @@ $(document).ready(function(){
       return false;
    });
 });
-document.onkeydown = function (e) {
-        return false;
-}
         </script>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
